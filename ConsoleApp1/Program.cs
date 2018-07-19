cbe6a583fd39f0325cd4e0832e330645c584194b
gperlaki <gperlaki@travix.com>
2018-07-19 13:54:59 +0200

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Loader;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var ended = new ManualResetEventSlim();
+            var starting = new ManualResetEventSlim();
+
+            AssemblyLoadContext.Default.Unloading += ctx =>
+            {
+                System.Console.WriteLine("Unloding fired");
+                starting.Set();
+                System.Console.WriteLine("Waiting for completion");
+                ended.Wait();
+            }; 
+
+            System.Console.WriteLine("Waiting for signals");
+            starting.Wait();
+
+            System.Console.WriteLine("Received signal gracefully shutting down");
+            Thread.Sleep(5000);
+            ended.Set();
+        }
+    }
+}
